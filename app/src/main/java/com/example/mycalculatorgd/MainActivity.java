dbe52668fefa9b49c38cfbc97f9009545badb838
RustamKZZ <kzrustam@gmail.com>
2021-09-24 00:25:11 +0300

    Homework04 - добавил стиль кнопок, размеры и строки в ресурсы, кнопки - материал, иконку

--- app/src/main/java/com/example/mycalculatorgd/MainActivity.java
+++ app/src/main/java/com/example/mycalculatorgd/MainActivity.java
@@ -3,31 +3,32 @@ package com.example.mycalculatorgd;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText playEt;
-    Button buttonOne;
-    Button buttonTwo;
-    Button buttonThree;
-    Button buttonFour;
-    Button buttonFive;
-    Button buttonSix;
-    Button buttonSeven;
-    Button buttonEight;
-    Button buttonNine;
-    Button buttonZero;
+    TextView playEt;
+    Button oneButton;
+    Button twoButton;
+    Button threeButton;
+    Button fourButton;
+    Button fiveButton;
+    Button sixButton;
+    Button sevenButton;
+    Button eightButton;
+    Button nineButton;
+    Button zeroButton;
 
-    Button buttonPlus;
-    Button buttonMinus;
-    Button buttonMultiply;
-    Button buttonDivision;
-    Button buttonDot;
-    Button buttonDelete;
-    Button buttonEqual;
+    Button plusButton;
+    Button minusButton;
+    Button multiplyButton;
+    Button divisionButton;
+    Button dotButton;
+    Button deleteButton;
+    Button equalButton;
 
     private String strNum = "";
     private double digitOne;
@@ -45,52 +46,52 @@ public class MainActivity extends AppCompatActivity {
             operation = savedInstanceState.getChar("saved_char_operation");
         }
 
-        playEt = findViewById(R.id.display_result);
-        buttonOne = findViewById(R.id.button_one);
-        buttonTwo = findViewById(R.id.button_two);
-        buttonThree = findViewById(R.id.button_three);
-        buttonFour = findViewById(R.id.button_four);
-        buttonFive = findViewById(R.id.button_five);
-        buttonSix = findViewById(R.id.button_six);
-        buttonSeven = findViewById(R.id.button_seven);
-        buttonEight = findViewById(R.id.button_eight);
-        buttonNine = findViewById(R.id.button_nine);
-        buttonZero = findViewById(R.id.button_zero);
+        playEt = findViewById(R.id.result_display);
+        oneButton = findViewById(R.id.one_button);
+        twoButton = findViewById(R.id.two_button);
+        threeButton = findViewById(R.id.three_button);
+        fourButton = findViewById(R.id.four_button);
+        fiveButton = findViewById(R.id.five_button);
+        sixButton = findViewById(R.id.six_button);
+        sevenButton = findViewById(R.id.seven_button);
+        eightButton = findViewById(R.id.eight_button);
+        nineButton = findViewById(R.id.nine_button);
+        zeroButton = findViewById(R.id.zero_button);
 
-        buttonPlus = findViewById(R.id.button_plus);
-        buttonMinus = findViewById(R.id.button_minus);
-        buttonMultiply = findViewById(R.id.button_multiply);
-        buttonDivision = findViewById(R.id.button_dev);
-        buttonDot = findViewById(R.id.button_dot);
-        buttonDelete = findViewById(R.id.button_delete);
-        buttonEqual = findViewById(R.id.button_equal);
+        plusButton = findViewById(R.id.plus_button);
+        minusButton = findViewById(R.id.minus_button);
+        multiplyButton = findViewById(R.id.multiply_button);
+        divisionButton = findViewById(R.id.dev_button);
+        dotButton = findViewById(R.id.dot_button);
+        deleteButton = findViewById(R.id.delete_button);
+        equalButton = findViewById(R.id.equal_button);
 
-        buttonOne.setOnClickListener(view -> addNumber(1));
-        buttonTwo.setOnClickListener(view -> addNumber(2));
-        buttonThree.setOnClickListener(view -> addNumber(3));
-        buttonFour.setOnClickListener(view -> addNumber(4));
-        buttonFive.setOnClickListener(view -> addNumber(5));
-        buttonSix.setOnClickListener(view -> addNumber(6));
-        buttonSeven.setOnClickListener(view -> addNumber(7));
-        buttonEight.setOnClickListener(view -> addNumber(8));
-        buttonNine.setOnClickListener(view -> addNumber(9));
-        buttonZero.setOnClickListener(view -> addNumber(0));
+        oneButton.setOnClickListener(view -> addNumber(1));
+        twoButton.setOnClickListener(view -> addNumber(2));
+        threeButton.setOnClickListener(view -> addNumber(3));
+        fourButton.setOnClickListener(view -> addNumber(4));
+        fiveButton.setOnClickListener(view -> addNumber(5));
+        sixButton.setOnClickListener(view -> addNumber(6));
+        sevenButton.setOnClickListener(view -> addNumber(7));
+        eightButton.setOnClickListener(view -> addNumber(8));
+        nineButton.setOnClickListener(view -> addNumber(9));
+        zeroButton.setOnClickListener(view -> addNumber(0));
 
-        buttonPlus.setOnClickListener(view -> mathAction('+'));
-        buttonMinus.setOnClickListener(view -> mathAction('-'));
-        buttonMultiply.setOnClickListener(view -> mathAction('*'));
-        buttonDivision.setOnClickListener(view -> mathAction('/'));
+        plusButton.setOnClickListener(view -> mathAction('+'));
+        minusButton.setOnClickListener(view -> mathAction('-'));
+        multiplyButton.setOnClickListener(view -> mathAction('*'));
+        divisionButton.setOnClickListener(view -> mathAction('/'));
 
-        buttonEqual.setOnClickListener(view -> equalCalculation());
+        equalButton.setOnClickListener(view -> equalCalculation());
 
-        buttonDot.setOnClickListener(view -> {
+        dotButton.setOnClickListener(view -> {
             if (!strNum.contains(".")) {
                 strNum += ".";
                 playEt.setText(strNum);
             }
         });
 
-        buttonDelete.setOnClickListener(view -> {
+        deleteButton.setOnClickListener(view -> {
             playEt.setText("");
             strNum = "";
             operation = 'h';
